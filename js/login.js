3a15f31d568c67a603be1f2c29e3fe12638b6cc5
meuge95 <maria_megan15@hotmail.com>
2021-08-16 12:06:17 -0300

    se vuelve a modificar el loging

--- js/login.js
+++ js/login.js
@@ -21,6 +21,28 @@ function verificacion(){
 
 }
 
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  let usuario={}
+
+  usuario.nombre=profile.getName();
+  usuario.estado="Conectado"
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+  location.href="index.html";
+  alert("Conctado");
+}
+
+
 function desconectar(){
     sessionStorage.clear();
     location.href="index.html"
