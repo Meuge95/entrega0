0cf0a81e6a94f53c2d051f69a9fc5ef540fc6d1f
meuge95 <maria_megan15@hotmail.com>
2021-08-16 11:31:57 -0300

    Se modifica el archivo

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+// key google= 13093693165-cochq5t90d0llc519nu97v7ck8uea9sf.apps.googleusercontent.com
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function verificacion(){
+    let dato = document.getElementById("user");
+    let usuario = {};
+    if(dato.value.trim() === ""){
+        alert ("Falta completar un campo");
+    }else {
+        location.href="index.html"
+        usuario.nombre = dato.value;
+
+        localStorage.setItem("usuario" , JSON.stringify(usuario));
+    }
+
+
+}
+
+function desconectar(){
+    sessionStorage.clear();
+    location.href="index.html"
+}
