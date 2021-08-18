b339e661b4261cd10b3b00e32d5d24e3b5a021a8
meuge95 <maria_megan15@hotmail.com>
2021-08-18 16:13:06 -0300

    se soluciona el login

--- js/login.js
+++ js/login.js
@@ -14,40 +14,17 @@ function verificacion(){
     if(dato.value.trim() === "" || contraseña.value.trim() ===""){
         alert ("Falta completar un campo");
     }else {
-        location.href="index.html"
+       
         usuario.nombre = dato.value;
-
+        usuario.estado ="conectado";
+        location.href="index.html";
+        
         localStorage.setItem("usuario" , JSON.stringify(usuario));
     }
 
 
 }
 
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
-  
-  let usuario={}
-
-  usuario.nombre=profile.getName();
-  usuario.estado="Conectado"
-  localStorage.setItem("usuario" , JSON.stringify(usuario));
-  
- 
-
-  // The ID token you need to pass to your backend:
-  /*var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);*/
-  location.href="index.html";
-  
-}
 
 
 function desconectar(){
